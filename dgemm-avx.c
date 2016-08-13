0d49d292fb8954259584afb81616008025e9596b
Hankun Zhao <jerryz123@berkeley.edu>
2016-08-12 18:50:13 -0700

    optimized

--- dgemm-avx.c
+++ dgemm-avx.c
@@ -4,7 +4,7 @@
 #define min(a,b) (((a)<(b))?(a):(b))
 
 const char* dgemm_desc = "Naive, three-loop dgemm.";
-const int L1_BLOCK_SIZE = 256;
+const int L1_BLOCK_SIZE = 32;
 const int L2_BLOCK_SIZE = 512;
 
 /* L1 Block size should be an increment of L2 block size */
@@ -72,98 +72,25 @@ void do_4x4_block(int M, int N, int K, int depth,  double* A, double* B, double*
   _mm256_storeu_pd(C2p, c2);
   _mm256_storeu_pd(C3p, c3);
 }
-
-/* void do_4x4_blockc(int M, int N, int K, int depth,  double* cA, double* cB, double* C) { */
-/*   __m256d bx0, bx1, bx2, bx3; */
-
-/*   __m256d acol; */
-
-/*   double* C0p = C; */
-/*   double* C1p = C + M; */
-/*   double* C2p = C + M * 2; */
-/*   double* C3p = C + M * 3; */
-  
-/*   __m256d c0 = _mm256_loadu_pd(C0p); */
-/*   __m256d c1 = _mm256_loadu_pd(C1p); */
-/*   __m256d c2 = _mm256_loadu_pd(C2p); */
-/*   __m256d c3 = _mm256_loadu_pd(C3p); */
-
-  
-
-/*   for (int i = 0; i < depth; i++) { */
-/*     acol = _mm256_loadu_pd(cA); */
-/*     cA += 4; */
-    
-/*     bx0 = _mm256_broadcast_sd(cB++); */
-/*     bx1 = _mm256_broadcast_sd(cB++); */
-/*     bx2 = _mm256_broadcast_sd(cB++); */
-/*     bx3 = _mm256_broadcast_sd(cB++); */
-
-/*     c0 = _mm256_add_pd(c0, _mm256_mul_pd(acol, bx0)); */
-/*     c1 = _mm256_add_pd(c1, _mm256_mul_pd(acol, bx1)); */
-/*     c2 = _mm256_add_pd(c2, _mm256_mul_pd(acol, bx2)); */
-/*     c3 = _mm256_add_pd(c3, _mm256_mul_pd(acol, bx3)); */
-
-    
-/*   } */
-
-/*   _mm256_storeu_pd(C0p, c0); */
-/*   _mm256_storeu_pd(C1p, c1); */
-/*   _mm256_storeu_pd(C2p, c2); */
-/*   _mm256_storeu_pd(C3p, c3); */
-/* } */
-
+/* A is M by K
+ * B is K by N
+ * C is M by N 
+ * idepth is number of rows in A to handle
+ * jdepth is number of columns in B to handle
+ * kdepth is number of columns in A, rows in B to handle
+ * A, B, C should point to (0, 0) location in each matrix, stored in column-major format */
 void do_l1_block(int M, int N, int K, int idepth, int jdepth, int kdepth, double* A, double* B, double* C) {
   
   int i, j, k;
   int rboundary = jdepth - (jdepth % 4);
   int bboundary = idepth - (idepth % 4);
 
-  /* double continuousA[bboundary * kdepth]; */
-  /* double continuousB[kdepth * 4]; */
-
-  /* double* cAp = continuousA; */
-  /* for (i = 0; i < bboundary; i += 4) { */
-   
-  /*   double* App = A + i; */
-  /*   for (k = 0; k < kdepth; k++) { */
-  /*     *cAp++ = App[0]; */
-  /*     *cAp++ = App[1]; */
-  /*     *cAp++ = App[2]; */
-  /*     *cAp++ = App[3]; */
-  /*     App = App + M; */
-  /*   } */
-  /* } */
-    
-  
-  
-  for (i = 0; i < bboundary; i += 4) {
-
-    /* double* cBp = continuousB; */
-
-    /* double* B0 = B + j * K; */
-    /* double* B1 = B + (j + 1) * K; */
-    /* double* B2 = B + (j + 2) * K; */
-    /* double* B3 = B + (j + 3) * K; */
-    
-    /* for (int n = 0; n < kdepth; n++) { */
-    /*   *cBp++ = *B0++; */
-    /*   *cBp++ = *B1++; */
-    /*   *cBp++ = *B2++; */
-    /*   *cBp++ = *B3++; */
-    /* } */
-    
-    for (j = 0; j < rboundary ; j += 4) {
-      for (k = 0; k < kdepth - 3; k += 4) {
- 	/* do_4x4_blockc(M, N, K, 4, continuousA + i * kdepth + k * 4, continuousB + 4 * k, C + i + j * M); */
-	do_4x4_block(M, N, K, 4, A + i + k * M, B + k + j * K, C + i + j * M);
-      }
-      if (kdepth - k > 0) {
-	/* do_4x4_blockc(M, N, K, kdepth - k, continuousA + i * kdepth + k * 4, continuousB + 4 * k, C + i + j * M); */
-      	do_4x4_block(M, N, K, kdepth - k, A + i + k * M, B + k + j * K, C + i + j * M);
-      }	
+  for (j = 0; j < rboundary; j += 4) {    
+    for (i = 0; i < bboundary ; i += 4) {
+      do_4x4_block(M, N, K, kdepth, A + i, B + j * K, C + i + j * M);
     }
   }
+  
   /* handle right fringe */
   for (int i = 0; i < bboundary; i++ ) {
     for (int j = rboundary; j < jdepth; j++ ) {
@@ -199,21 +126,21 @@ void do_l1_block(int M, int N, int K, int idepth, int jdepth, int kdepth, double
 void dgemm (int M, int N, int K, double* A, double* B, double* C) {
   /* L2 Blocking */
   for (int k2 = 0; k2 < K; k2 += L2_BLOCK_SIZE) {
-    int k2depth = min(L2_BLOCK_SIZE, K - k2);
+    int k2end = min(k2 + L2_BLOCK_SIZE, K);
     for (int j2 = 0; j2 < N; j2 += L2_BLOCK_SIZE) {
-      int j2depth = min(L2_BLOCK_SIZE, N - j2);
+      int j2end = min(j2 + L2_BLOCK_SIZE, N);
       for (int i2 = 0; i2 < M; i2 += L2_BLOCK_SIZE) {
-	int i2depth = min(L2_BLOCK_SIZE, M - i2);
+	int i2end = min(i2 + L2_BLOCK_SIZE, M);
 
 	/* L1 Blocking */
-        for (int k1 = 0; k1 < k2depth; k1 += L1_BLOCK_SIZE) {
-	  int k1depth = min(L1_BLOCK_SIZE, k2depth - k1);
-	  for (int j1 = 0; j1 < j2depth; j1 += L1_BLOCK_SIZE) {
-	    int j1depth = min(L1_BLOCK_SIZE, j2depth - j1);
-	    for (int i1 = 0; i1 < i2depth; i1 += L1_BLOCK_SIZE) {
-	      int i1depth = min(L1_BLOCK_SIZE, i2depth - i1);
+        for (int k1 = k2; k1 < k2end; k1 += L1_BLOCK_SIZE) {
+	  int k1depth = min(L1_BLOCK_SIZE, k2end - k1);
+	  for (int j1 = j2; j1 < j2end; j1 += L1_BLOCK_SIZE) {
+	    int j1depth = min(L1_BLOCK_SIZE, j2end - j1);
+	    for (int i1 = i2; i1 < i2end; i1 += L1_BLOCK_SIZE) {
+	      int i1depth = min(L1_BLOCK_SIZE, i2end - i1);
 
-	      do_l1_block(M, N, K, i1depth, j1depth, k1depth, A + i1 + i2 + (k1 + k2) * M, B + k1 + k2 + (j1 + j2) * K, C + i1 + i2 + (j1 + j2) * M);
+	      do_l1_block(M, N, K, i1depth, j1depth, k1depth, A + i1 + k1 * M, B + k1 + j1 * K, C + i1 + j1 * M);
 
 	    }
 	  }
