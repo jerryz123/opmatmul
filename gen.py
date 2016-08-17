cfad8fddedb4a665c370ab20ceb50a3b4b4aa1ed
jerryz123 <jerryz123@berkeley.edu>
2016-08-17 14:15:06 -0700

    added gener

---
+++ gen.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+with open("naive.txt", "r") as f:
+    s = f.read()
+    s = s.split("\n")
+    s = [x.split("\t") for x in s[3:-1]]
+    xs = [int(x[0]) for x in s]
+    y1s = [float(x[5]) for x in s]
+with open("avx.txt", "r") as f:
+    t = f.read()
+    t = t.split("\n")
+    t = [x.split("\t") for x in t[3:-1]]
+    y2s = [float(x[5]) for x in t]
+
+plt.plot(xs, y1s, 'r--',  y2s, 'b--')
+plt.savefig("comparison.png")
